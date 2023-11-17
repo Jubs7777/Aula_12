ec3477964bf825dbb403a3646483031001ffe5d1
Juliana <lealjubs7@gmail.com>
2023-11-16 21:20:15 -0300

    aula php
    Testei o php

---
+++ aula12.php
@@ -0,0 +1,14 @@
+<?php 
+
+$nome = "Juliana";
+$sobrenome = "Leal";
+$cidade = "São Paulo";
+$idade = "18 anos";
+$cor = "roxo e preto";
+$estado = "SP";
+$local = "Rolê cultural";
+
+echo "Meu nome é $nome $sobrenome moro na $cidade de $estado, tenho $idade, minha cor favorita é $cor e eu gosto sair para $local"
+
+
+?>
